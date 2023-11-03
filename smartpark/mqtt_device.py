bb0cd5e5014be604b05f6da5dead8e8fa96e2253
ca20110820 <ca20110820@gmail.com>
2023-11-03 13:57:32 +0800

    Implemented MqttDevice

--- smartpark/mqtt_device.py
+++ smartpark/mqtt_device.py
@@ -1,24 +1,26 @@
 import paho.mqtt.client as paho
+
+
 class MqttDevice:
-    def __init__(self, config):
-        self.name = config['name']
-        self.location = config['location']
+    def __init__(self, config: dict, keepalive: int = 65535, *args, **kwargs):
+        self.topic_root = config["topic-root"]
+        self.location = config["location"]
+        self.name = config["name"]
+        self.topic_qualifier = config["topic-qualifier"]
+        self.host = config["host"]
+        self.port = config["port"]
 
-        # Define topic components:
-        self.topic_root = config['topic-root']
-        self.topic_qualifier = config['topic-qualifier']
-        self.topic = self._create_topic_string()
+        self.client: paho.Client = paho.Client(*args, **kwargs)
+        self.client.connect(self.host, self.port, keepalive=keepalive)
 
-        # Configure broker
-        self.broker = config['broker']
-        self.port = config['port']
+    @property
+    def topic_address(self) -> str:
+        # Default Topic: <topic-root>/<location>/<name>/<topic-qualifier>
+        return f"{self.topic_root}/{self.location}/{self.name}/{self.topic_qualifier}"
 
-        # initialise a paho client and bind it to the object (has-a)
+    def create_topic_address(self, custom_location: str, custom_name: str, custom_topic_qualifier: str):
+        # Note: Topic Root cannot be changed.
+        return f"{self.topic_root}/{custom_location}/{custom_name}/{custom_topic_qualifier}"
 
-        self.client = paho.Client()
-        self.client.connect(self.broker,
-                            self.port)
-
-    def _create_topic_string(self):
-        return (f"{self.topic_root}/{self.location}/" +
-                f"{self.name}/{self.topic_qualifier}")
+    def create_topic_qualifier(self, custom_topic_qualifier: str):
+        return f"{self.topic_root}/{self.location}/{self.name}/{custom_topic_qualifier}"
