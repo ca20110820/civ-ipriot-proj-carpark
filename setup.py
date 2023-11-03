582c706799c50832391cd3078d31008251146e21
ca20110820 <ca20110820@gmail.com>
2023-11-03 11:46:57 +0800

    Initial Project Setup

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name="smartpark",
     version="0.1.0",
+    description="IP4RIoT - AT3 Project",
+    author="Cedric Anover",
     packages=find_packages(),
     install_requires=[
         "paho-mqtt",
         "sense-hat",
-        "tkinter",
         "toml"
     ],
     entry_points={
@@ -15,5 +16,5 @@ setup(
             "smartpark = smartpark.main:main",
         ],
     },
-    python_requires=">=3.10",
+    python_requires=">=3.10"
 )
