9fda4653fc0a7ecb87206dbea900af6030d641a4
ca20110820 <ca20110820@gmail.com>
2023-11-28 10:09:33 +0800

    Updated unit test methods for CarPark and made it specific for important class methods

--- tests/test_car_park.py
+++ tests/test_car_park.py
@@ -70,6 +70,108 @@ class TestCarPark(unittest.TestCase):
         self.detector = MockDetector()
         self.fixed_num_bays = self.config.get_car_park_config(self.car_park_name)["total_bays"]
 
+    def test_car_park_initialized_with_all_attributes(self):
+        self.assertIsInstance(self.car_park, CarPark)
+        self.assertIsNone(self.car_park.temperature)
+        self.assertIsNone(self.car_park.entry_or_exit_time)
+        self.assertEqual(self.car_park.available_bays, 5)
+        self.assertEqual(self.car_park.total_cars, 0)
+        self.assertEqual(self.car_park.location, "Moondaloop Park")
+
+    def test_add_car(self):
+        car = Car.generate_random_car(["ModelA", "ModelB", "ModelC"])
+        self.car_park.temperature = 23  # Set entry temperature
+
+        self.car_park.add_car(car)
+
+        self.assertEqual(self.car_park.total_cars, 1)
+        self.assertEqual(self.car_park.parked_cars, 0)
+        self.assertEqual(self.car_park.un_parked_cars, 1)
+        self.assertEqual(self.car_park.available_bays, 5)
+        self.assertIn(car, self.car_park.get_un_parked_cars())
+        self.assertNotIn(car, self.car_park.get_parked_cars())
+        self.assertEqual(car.entry_time, self.car_park.entry_or_exit_time)
+        self.assertAlmostEqual(car.entry_temperature, self.car_park.temperature)
+
+        car.car_parked()
+
+        self.assertEqual(self.car_park.parked_cars, 1)
+        self.assertEqual(self.car_park.un_parked_cars, 0)
+        self.assertEqual(self.car_park.available_bays, 4)
+        self.assertNotIn(car, self.car_park.get_un_parked_cars())
+        self.assertIn(car, self.car_park.get_parked_cars())
+
+    def test_remove_car(self):
+        car = Car.generate_random_car(["ModelA", "ModelB", "ModelC"])
+        self.car_park.temperature = 23  # Set entry temperature
+        self.car_park.add_car(car)
+        car.car_parked()
+
+        car.car_unparked()
+        self.car_park.temperature = 24  # Set exit temperature
+        self.car_park.remove_car(car)
+
+        self.assertEqual(self.car_park.total_cars, 0)
+        self.assertEqual(self.car_park.parked_cars, 0)
+        self.assertEqual(self.car_park.un_parked_cars, 0)
+        self.assertEqual(self.car_park.available_bays, 5)
+        self.assertNotIn(car, self.car_park.get_un_parked_cars())
+        self.assertNotIn(car, self.car_park.get_parked_cars())
+        self.assertEqual(car.entry_time, self.car_park.entry_or_exit_time)
+        self.assertAlmostEqual(car.exit_temperature, self.car_park.temperature)
+
+    def test_on_car_entry(self):
+        self.car_park.temperature = random.randint(20, 30)  # Set entry temperature
+        self.car_park.on_car_entry()
+
+        self.assertEqual(self.car_park.total_cars, 1)
+        self.assertEqual(self.car_park.available_bays, 4)
+        self.assertEqual(self.car_park.parked_cars, 1)
+        self.assertEqual(self.car_park.un_parked_cars, 0)
+
+        for car in self.car_park.get_all_cars():
+            self.assertIsInstance(car.entry_temperature, float)
+            self.assertIsInstance(car.entry_time, datetime)
+            self.assertIsNone(car.exit_temperature)
+            self.assertIsNone(car.exit_time)
+
+    def test_on_car_exit(self):
+        self.car_park.temperature = random.randint(20, 30)  # Set entry temperature
+        self.car_park.on_car_entry()
+
+        self.car_park.temperature = random.randint(20, 30)  # Set entry temperature
+        exited_car = self.car_park.on_car_exit()
+
+        self.assertIsInstance(exited_car, Car)
+        self.assertIsInstance(exited_car.entry_temperature, float)
+        self.assertIsInstance(exited_car.entry_time, datetime)
+        self.assertIsInstance(exited_car.exit_temperature, float)
+        self.assertIsInstance(exited_car.exit_time, datetime)
+
+        self.assertNotIn(exited_car, self.car_park.get_all_cars())
+        self.assertNotIn(exited_car, self.car_park.get_parked_cars())
+        self.assertNotIn(exited_car, self.car_park.get_un_parked_cars())
+
+    def test_overfill_the_car_park(self):
+        for _ in range(5):
+            self.car_park.temperature = random.randint(20, 30)  # Set entry temperature
+            self.car_park.on_car_entry()
+
+        self.assertEqual(self.car_park.total_cars, 5)
+        self.assertEqual(self.car_park.parked_cars, 5)
+        self.assertEqual(self.car_park.un_parked_cars, 0)
+        self.assertEqual(self.car_park.available_bays, 0)
+
+        # Add a new car to a full car park
+        new_car = self.car_park.on_car_entry()
+
+        self.assertEqual(self.car_park.total_cars, 6)
+        self.assertEqual(self.car_park.parked_cars, 5)
+        self.assertEqual(self.car_park.un_parked_cars, 1)
+        self.assertEqual(self.car_park.available_bays, 0)
+        self.assertIn(new_car, self.car_park.get_un_parked_cars())
+        self.assertNotIn(new_car, self.car_park.get_parked_cars())
+
     def test_on_message(self):
         """Test the State of CarPark when a Car Entered/Exited"""
         counter = 0
